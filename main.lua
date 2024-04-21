af388add7256c54b424559d589fbed6e67bbbc76
Miguel <39136142+LoveBetween@users.noreply.github.com>
2024-04-22 01:41:26 +0200

    Update main.lua

--- main.lua
+++ main.lua
@@ -1,46 +1,67 @@
 log.info("Successfully loaded ".._ENV["!guid"]..".")
 mods.on_all_mods_loaded(function() for k, v in pairs(mods) do if type(v) == "table" and v.hfuncs then Helper = v end end end)
 
--- Testing grenades
-
-local grenade_table = {}
-local count = 0
-
 -- Parameters
 
 local base_speed = 4
-local speed_up = 1.2
+local speed_up = 1.4
 local damage_coeff = 0.7
 local throw_gravity = 0.25
 local throw_speed = -4.5
 local hit_distance = 50
+local max_ball_bounces = 10
 
-function create_ball(old, parent)
+
+---------
+
+local old_variables = {}
+local old_balls = {}
+
+function create_ball(parent)
     -- Grenade is recreated
-    if old ~= nil then
-        if old.hspeed >0 then
-            ball = gm.instance_create_depth(old.x - old.hspeed*10, old.y, 1, 681)
-        else
-            ball = gm.instance_create_depth(old.x - old.hspeed*10, old.y, 1, 681)
-        end
-        ball.hspeed = -old.hspeed
-        ball.parent = parent
-        ball.is_ball = true, 
-        print(ball)
-        return ball
-    else -- Grenade is new and thrown up
-        local ball = gm.instance_create_depth(parent.x, parent.y, 1, 681)
-        print(parent.y)
-        ball.parent = parent
-        ball.gravity = throw_gravity
-        ball.vspeed =  throw_speed
-        ball.bounces = -1000
-        ball.is_ball, ball.status = true, "bunted_up"
-        ball.old_vspeed, ball.old_hspeed = throw_speed, 0.0
-        ball.damage_coeff = damage_coeff
-        print(ball.id)
-        return ball
-    end
+    local ball = gm.instance_create_depth(parent.x, parent.y, 1, 681)
+    ball.parent = parent
+    --transform
+    ball.gravity = throw_gravity
+    ball.vspeed, ball.hspeed =  throw_speed, 0.0
+    ball.old_vspeed, ball.old_hspeed= throw_speed, 0.0
+    --properties
+    ball.bounces = - max_ball_bounces + 3
+    ball.is_ball, ball.status = true, "bunted_up"
+    ball.damage_coeff = damage_coeff
+
+    old_variables[#old_variables + 1] = get_ball_vars(ball)
+    return ball
+end
+
+function recreate_ball(old_var, old_index)
+    ball = gm.instance_create_depth(old_var.x , old_var.y, 1, 681)
+    ball.parent = old_var.par
+    -- transform
+    ball.gravity = 0
+    ball.hspeed, ball.vspeed = -old_var.old_hspeed, old_var.old_vspeed
+    ball.old_hspeed, ball.old_vspeed = -old_var.old_hspeed, old_var.old_vspeed
+    -- --properties
+    ball.bounces = old_var.bounces - 1
+    ball.is_ball, ball.status = true, "hit"
+    ball.damage_coeff = old_var.damage_coeff
+
+    old_variables[old_index] = get_ball_vars(ball)
+
+    return ball
+end
+
+function get_ball_vars(ball)
+    return {
+        x = ball.x,
+        y = ball.y,
+        par = ball.parent,
+        old_hspeed = ball.old_hspeed, 
+        old_vspeed = ball.old_vspeed,
+        bounces = ball.bounces,
+        status = ball.status,
+        is_ball = ball.is_ball,
+    }
 end
 
 function find_closest_ball(parent)
@@ -89,21 +110,29 @@ function hit_ball(ball, parent, distance)
     ball.status = "hit"
 end
 
+find_balls_all = function()
+    local balls = {}
+    local ball = nil
+    for i = 0, gm.instance_number(gm.constants.oEngiGrenade) - 1 do
+        ball = gm.instance_find(gm.constants.oEngiGrenade, i)
+        if ball.status then table.insert(balls, ball) end
+    end
+    return balls, #balls > 0
+end
 
-local old_balls = {}
 function update_balls()
-    local balls, balls_exist = Helper.find_active_instance_all(gm.constants.oEngiGrenade)
-    if not balls_exist then return nil end
+    local balls, balls_exist = find_balls_all(gm.constants.oEngiGrenade)
+    --if not balls_exist then return nil end
     for i=1, #balls do
         if balls[i].status == "hit" then
-            if math.abs(balls[i].hspeed) < 2 then --
+            if math.abs(balls[i].hspeed) < 2 then
                 balls[i].x = balls[i].x - 2 * balls[i].old_hspeed
                 balls[i].hspeed = - balls[i].old_hspeed
                 balls[i].vspeed = balls[i].old_vspeed 
                 balls[i].damage_coeff = damage_coeff * math.abs(balls[i].hspeed)
             end
         elseif balls[i].status == "bunted_up" then
-            if balls[i].vspeed < 1.0 then
+            if balls[i].vspeed < -1.0 then
                 balls[i].status = "bunted_top"
             end
         elseif balls[i].status == "bunted_top" then
@@ -121,31 +150,23 @@ function update_balls()
         end
         balls[i].old_hspeed = balls[i].hspeed
     end
+
     if #old_balls ~= #balls then
-        print("ball destroyed")
-        
-
-
+        local index = 1
+        for old_index = 1, #old_balls do
+            if not balls[index] or old_balls[old_index].id ~= balls[index].id then
+                local old_ball = old_balls[old_index]
+                recreate_ball(old_variables[old_index], old_index)
+            else
+                index = index + 1
+            end
+        end
     end
     old_balls = balls
-
-end
-
-function check_grenades()
-    for index, grenade in pairs(grenade_table) do
-        --print(grenade.c.." - "..count)
-        if grenade.c < count and grenade.c >0 then
-            grenade.c = -1
-            -- x,y, ?, object index
-            local inst = nil
-            inst = create_ball (grenade.obj, client_player)
-            grenade_table[inst.id] = {
-                obj = inst,
-                c = count
-            }
-            --table.remove(grenade_table, index) 
-            print(#grenade_table)
-        end
+    old_variables = {}
+    -- variables[] don't stay on object destroy 
+    for i = 1, #balls do
+        old_variables[i] = get_ball_vars(balls[i])
     end
 end
 
@@ -154,14 +175,13 @@ gm.post_script_hook(gm.constants.skill_activate, function(self, other, result, a
     client_player = Helper.get_client_player() -- get the client
     if self ~= client_player then return end -- is the client using the skill
     if args[1].value == 0 then -- ball throw
-        create_ball (nil, client_player)
+        create_ball (client_player)
     end 
     if args[1].value == 1 then -- ball hit
         local ball, distance = find_closest_ball(client_player)
         if ball ~= nil then hit_ball(ball, client_player, distance) end
     end
 end)
-
 
 gm.post_script_hook(gm.constants.__input_system_tick, function() 
     update_balls()
