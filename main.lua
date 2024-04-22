cfaec292824332b28f9591362d7aaa67a9dad851
Miguel <39136142+LoveBetween@users.noreply.github.com>
2024-04-22 23:50:26 +0200

    Update main.lua

--- main.lua
+++ main.lua
@@ -10,17 +10,19 @@ local throw_gravity = 0.25
 local throw_speed = -4.5
 local hit_distance = 50
 local max_ball_bounces = 10
+local custom_id = 0
 
 
 ---------
 
 local old_variables = {}
-local old_balls = {}
 
 function create_ball(parent)
     -- Grenade is recreated
     local ball = gm.instance_create_depth(parent.x, parent.y, 1, 681)
     ball.parent = parent
+    ball.custom_id = custom_id
+    custom_id = custom_id + 1
     --transform
     ball.gravity = throw_gravity
     ball.vspeed, ball.hspeed =  throw_speed, 0.0
@@ -30,23 +32,22 @@ function create_ball(parent)
     ball.is_ball, ball.status = true, "bunted_up"
     ball.damage_coeff = damage_coeff
 
-    old_variables[#old_variables + 1] = get_ball_vars(ball)
+    old_variables[ball.custom_id] = get_ball_vars(ball)
     return ball
 end
 
 function recreate_ball(old_var, old_index)
-    ball = gm.instance_create_depth(old_var.x , old_var.y, 1, 681)
+    ball = gm.instance_create_depth(old_var.x - old_var.old_hspeed * 5, old_var.y, 1, 681)
     ball.parent = old_var.par
+    ball.custom_id = old_var.custom_id
     -- transform
     ball.gravity = 0
     ball.hspeed, ball.vspeed = -old_var.old_hspeed, old_var.old_vspeed
     ball.old_hspeed, ball.old_vspeed = -old_var.old_hspeed, old_var.old_vspeed
     -- --properties
-    ball.bounces = old_var.bounces - 1
+    ball.bounces = old_var.bounces + 1
     ball.is_ball, ball.status = true, "hit"
-    ball.damage_coeff = old_var.damage_coeff
-
-    old_variables[old_index] = get_ball_vars(ball)
+    ball.damage_coeff = damage_coeff * math.abs(ball.hspeed)
 
     return ball
 end
@@ -61,6 +62,7 @@ function get_ball_vars(ball)
         bounces = ball.bounces,
         status = ball.status,
         is_ball = ball.is_ball,
+        custom_id = ball.custom_id,
     }
 end
 
@@ -69,7 +71,7 @@ function find_closest_ball(parent)
     if not balls_exist then return nil end
     local closest_ball = nil
     local distance  = 0 
-    local closest_distance = 10000000000
+    local closest_distance = 10000000000000
     for i=1, #balls do
         if balls[i].is_ball then
             distance = (balls[i].x - parent.x)*(balls[i].x - parent.x) + (balls[i].y - parent.y)*(balls[i].y - parent.y) -- clean this later
@@ -106,68 +108,76 @@ function hit_ball(ball, parent, distance)
     ball.old_vspeed = 0
     ball.old_hspeed = ball.hspeed
     ball.gravity = 0
-    ball.damage_coeff = damage_coeff * speed
+    ball.damage_coeff = damage_coeff * math.abs(ball.hspeed)
     ball.status = "hit"
 end
 
-find_balls_all = function()
+function find_balls_all()
     local balls = {}
     local ball = nil
     for i = 0, gm.instance_number(gm.constants.oEngiGrenade) - 1 do
         ball = gm.instance_find(gm.constants.oEngiGrenade, i)
-        if ball.status then table.insert(balls, ball) end
+        if ball ~=nil and ball.status then balls[ball.custom_id] = ball end
     end
-    return balls, #balls > 0
+    return balls, next(balls) ~= nil
 end
 
-function update_balls()
-    local balls, balls_exist = find_balls_all(gm.constants.oEngiGrenade)
-    --if not balls_exist then return nil end
-    for i=1, #balls do
-        if balls[i].status == "hit" then
-            if math.abs(balls[i].hspeed) < 2 then
-                balls[i].x = balls[i].x - 2 * balls[i].old_hspeed
-                balls[i].hspeed = - balls[i].old_hspeed
-                balls[i].vspeed = balls[i].old_vspeed 
-                balls[i].damage_coeff = damage_coeff * math.abs(balls[i].hspeed)
+function update_balls() 
+    local balls, balls_exist = find_balls_all()
+    if not balls_exist and next(old_variables) == nil then return nil end -- if no old or new balls exist
+    
+    -- ball bounce and status
+    for c_id, ball in pairs(balls) do
+        if ball.status == "hit" then
+            if math.abs(ball.hspeed) < 2 then
+                ball.x = ball.x - 2 * ball.old_hspeed
+                ball.hspeed = - ball.old_hspeed
+                ball.vspeed = ball.old_vspeed 
+                ball.damage_coeff = damage_coeff * math.abs(ball.hspeed)
             end
-        elseif balls[i].status == "bunted_up" then
-            if balls[i].vspeed < -1.0 then
-                balls[i].status = "bunted_top"
+        elseif ball.status == "bunted_up" then
+            if ball.vspeed < -1.0 then
+                ball.status = "bunted_top"
             end
-        elseif balls[i].status == "bunted_top" then
-            if balls[i].vspeed > 1.0 then
-                balls[i].status = "bunted_down"
+        elseif ball.status == "bunted_top" then
+            if ball.vspeed > 1.0 then
+                ball.status = "bunted_down"
             end
-        elseif balls[i].status == "bunted_down" then
-            if balls[i].vspeed < 1.0 then
-                balls[i].status = "grounded"
+        elseif ball.status == "bunted_down" then
+            if ball.vspeed < 1.0 then
+                ball.status = "grounded"
             end
-        elseif balls[i].status == "grounded" then
-            balls[i].gravity = 0
-            balls[i].hspeed = 0
-            balls[i].vspeed = 0
+        elseif ball.status == "grounded" then
+            ball.gravity = 0
+            ball.hspeed = 0
+            ball.vspeed = 0
         end
-        balls[i].old_hspeed = balls[i].hspeed
+        ball.old_hspeed = ball.hspeed
     end
 
-    if #old_balls ~= #balls then
-        local index = 1
-        for old_index = 1, #old_balls do
-            if not balls[index] or old_balls[old_index].id ~= balls[index].id then
-                local old_ball = old_balls[old_index]
-                recreate_ball(old_variables[old_index], old_index)
-            else
+    -- Save ball info and recreate balls when they hit and ennemy
+    new_variables = {}
+    local index = 1
+    for c_id, old_values in pairs(old_variables) do
+        if old_values.bounces < 3 -- grenades explode on 3rd bounce
+        and old_values.status == "hit" -- don't bounce ball against ennemies if it's bunted
+        and not balls[c_id] -- ball doesnt exist anymore
+        then
+            local reborn_ball = recreate_ball(old_variables[c_id], old_index)
+            --print(reborn_ball)
+            new_variables[c_id] = get_ball_vars(reborn_ball)
+        else
+            if balls[c_id] ~= nil then
+                new_variables[c_id] = balls[c_id]
                 index = index + 1
             end
         end
     end
-    old_balls = balls
-    old_variables = {}
-    -- variables[] don't stay on object destroy 
-    for i = 1, #balls do
-        old_variables[i] = get_ball_vars(balls[i])
+    for c_id, values in pairs(balls) do
+        new_variables[c_id] = get_ball_vars(balls[c_id])
     end
+    --debug
+    old_variables = new_variables
 end
 
 local client_player = nil
@@ -185,4 +195,4 @@ end)
 
 gm.post_script_hook(gm.constants.__input_system_tick, function() 
     update_balls()
-end) 
+end)
